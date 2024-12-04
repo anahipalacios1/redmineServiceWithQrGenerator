d448102fa67858d788409dfd28735d022df142d1
palaciosdominguezani <palaciosdominguezani@fpuna.edu.py>
2024-12-04 16:41:29 -0300

    ajustes de pdf frontal y dorsal

--- src/main/java/com/mycompany/qrcode/controllers/IssueController.java
+++ src/main/java/com/mycompany/qrcode/controllers/IssueController.java
@@ -1,8 +1,10 @@
 package com.mycompany.qrcode.controllers;
 
+import com.mycompany.qrcode.beans.CustomField;
 import com.mycompany.qrcode.response.IssuesResponse;
 import com.mycompany.qrcode.services.RedmineService;
 import com.mycompany.qrcode.beans.Issue;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ContentDisposition;
 import org.springframework.http.HttpHeaders;
@@ -50,19 +52,46 @@ public class IssueController {
     @GetMapping("/front/pdf/{id}")
     public ResponseEntity<byte[]> exportPdfForIssueFront(@PathVariable("id") int id) {
         try {
+            // Obtener los issues desde el servicio Redmine
             IssuesResponse issuesResponse = redmineService.getIssues();
+
+            // Buscar el issue específico por ID
             Issue issue = issuesResponse.getIssues().stream()
                     .filter(i -> i.getId() == id)
                     .findFirst()
                     .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Issue not found"));
-            byte[] pdfBytes = redmineService.exportReportFront(issue);
+
+            // Obtener los campos personalizados
+            List<CustomField> customFields = issue.getCustomFields();
+
+            // Extraer el valor del campo "Fotografía"
+            String fotoId = (String) customFields.stream()
+                    .filter(cf -> cf.getName().equals("Fotografía"))
+                    .map(CustomField::getValue)
+                    .findFirst()
+                    .orElse(null);
+
+            byte[] fotografia = null;
+            if (fotoId != null) {
+                // Obtener la imagen desde Redmine
+                fotografia = redmineService.obtenerImagenDesdeRedmine(fotoId);
+            }
+
+            // Llamar al servicio para generar el PDF, pasando solo el Issue
+            byte[] pdfBytes = redmineService.exportReportFront(issue); // Solo el issue
+
+            if (pdfBytes == null || pdfBytes.length == 0) {
+                throw new RuntimeException("El PDF generado está vacío.");
+            }
+
+            // Configurar encabezados para la respuesta
             HttpHeaders headers = new HttpHeaders();
             headers.setContentType(MediaType.APPLICATION_PDF);
             headers.setContentDisposition(ContentDisposition.builder("attachment")
                     .filename("issue_" + id + "_report.pdf")
                     .build());
-            return new ResponseEntity<>(pdfBytes, headers, HttpStatus.OK);
 
+            return new ResponseEntity<>(pdfBytes, headers, HttpStatus.OK);
         } catch (Exception e) {
             e.printStackTrace();
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
--- src/main/java/com/mycompany/qrcode/services/RedmineService.java
+++ src/main/java/com/mycompany/qrcode/services/RedmineService.java
@@ -15,13 +15,18 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.mycompany.qrcode.beans.CustomField;
 import com.mycompany.qrcode.beans.Issue;
 import com.mycompany.qrcode.util.IssuesReportGenerator;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import javax.imageio.ImageIO;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperCompileManager;
 import net.sf.jasperreports.engine.JasperExportManager;
@@ -29,6 +34,7 @@ import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import org.springframework.http.HttpStatus;
 import org.springframework.util.ResourceUtils;
 
 @Service
@@ -66,7 +72,7 @@ public class RedmineService {
         }
     }
 
-    public byte[] exportReportFront(Issue issue) throws JRException, FileNotFoundException {
+    public byte[] exportReportFront(Issue issue) throws JRException, IOException {
         File file = ResourceUtils.getFile("classpath:employees.jrxml");
         JasperReport jasperReport = JasperCompileManager.compileReport(file.getAbsolutePath());
         List<Issue> issueList = new ArrayList<>();
@@ -80,13 +86,82 @@ public class RedmineService {
                 || cf.getName().equals("Sector")
                 || cf.getName().equals("Codigo de Personal"))
                 .collect(Collectors.toList());
+
         JRBeanCollectionDataSource customFieldsDataSource = new JRBeanCollectionDataSource(filteredCustomFields);
+        Object fotoId = customFields.stream()
+                .filter(cf -> cf.getName().equals("Fotografía"))
+                .map(CustomField::getValue)
+                .findFirst()
+                .orElse(null);
+        byte[] fotografia = null;
+        if (fotoId != null) {
+            fotografia = obtenerImagenDesdeRedmine(fotoId);
+        }
         Map<String, Object> parameters = new HashMap<>();
         parameters.put("createdBy", "Java Developer");
         parameters.put("CUSTOM_FIELDS_DATASOURCE", customFieldsDataSource);
         parameters.put("SUBREPORT_PATH", "C:/Users/Usuario/JaspersoftWorkspace/MyReports/");
+        parameters.put("PHOTO", fotografia);
         JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, issueDataSource);
         return JasperExportManager.exportReportToPdf(jasperPrint);
+    }
+
+    public byte[] obtenerImagenDesdeRedmine(Object fotoId) throws IOException {
+        String url = "https://redmine4.sudolabs.com.py/attachments/download/" + fotoId;
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("X-Redmine-API-Key", redmineConfig.getKey());
+
+        HttpEntity<Void> requestEntity = new HttpEntity<>(headers);
+
+        try {
+            // Realizar la solicitud GET
+            ResponseEntity<byte[]> response = restTemplate.exchange(
+                    url,
+                    HttpMethod.GET,
+                    requestEntity,
+                    byte[].class
+            );
+
+            // Verificar el tipo de contenido
+            String contentType = response.getHeaders().getContentType().toString();
+            System.out.println("Response Headers: " + response.getHeaders());
+
+            if (response.getStatusCode() == HttpStatus.OK && response.getBody() != null) {
+                byte[] imageBytes = response.getBody();
+
+                // Verificar si el tipo de contenido es una imagen
+                if (!contentType.startsWith("image/")) {
+                    String errorHtml = new String(response.getBody(), StandardCharsets.UTF_8);
+                    throw new IOException("La respuesta no es una imagen. Content-Type: " + contentType + ". Respuesta HTML: " + errorHtml);
+                }
+
+                // Verificar si la imagen es válida
+                if (isValidImage(imageBytes)) {
+                    return imageBytes;
+                } else {
+                    throw new IOException("La imagen descargada no es válida.");
+                }
+            } else {
+                // Capturar el cuerpo HTML si la respuesta no es válida
+                String errorHtml = new String(response.getBody(), StandardCharsets.UTF_8);
+                throw new IOException("No se pudo descargar la imagen desde Redmine. Código de estado: " + response.getStatusCode() + ". Respuesta HTML: " + errorHtml);
+            }
+        } catch (Exception e) {
+            throw new IOException("Error al obtener la imagen desde Redmine: " + e.getMessage(), e);
+        }
+    }
+
+// Método para verificar si los bytes corresponden a una imagen válida
+    private boolean isValidImage(byte[] imageBytes) {
+        try {
+            ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(imageBytes);
+            BufferedImage bufferedImage = ImageIO.read(byteArrayInputStream);
+            return bufferedImage != null; // Si no es null, es una imagen válida
+        } catch (IOException e) {
+            return false; // Si ocurre un error, no es una imagen válida
+        }
     }
 
     public byte[] exportReportBack(Issue issue) throws JRException, FileNotFoundException {
