c736b50f27961d0918681c14ad6b936e71974ada
anahipalacios1 <palaciosdominguezani@gmail.com>
2025-01-21 15:18:43 -0300

    varios ajustes

--- src/main/java/com/mycompany/qrcode/controllers/QrCodeController.java
+++ src/main/java/com/mycompany/qrcode/controllers/QrCodeController.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import jakarta.servlet.http.HttpServletResponse;
 import javax.imageio.ImageIO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,10 +20,13 @@ public class QrCodeController {
     @Autowired
     private QrCodeService qrCodeService;
 
+    @Value("app.url")
+    private String appUrl;
+
     @GetMapping("/qr/{id}")
     public void getQrCode(@PathVariable("id") int id, HttpServletResponse response) throws IOException, WriterException, Exception {
         try {
-            String qrUrl = "http://localhost:8080/issue?id=" + id;
+            String qrUrl = appUrl + "issue?id=" + id;
             BufferedImage qrImage = qrCodeService.generateQrCode(qrUrl);
             response.setContentType("image/png");
             ImageIO.write(qrImage, "PNG", response.getOutputStream());
