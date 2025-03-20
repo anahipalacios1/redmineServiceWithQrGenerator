ece76d805f1d1ea15f2b740f695398665804891d
anahipalacios1 <palaciosdominguezani@gmail.com>
2025-03-20 20:52:06 -0300

    ajustes de diseño

--- src/main/java/com/mycompany/qrcode/util/IssuesReportGenerator.java
+++ src/main/java/com/mycompany/qrcode/util/IssuesReportGenerator.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.ResourceUtils;
 import com.mycompany.qrcode.beans.Issue;
 import com.mycompany.qrcode.response.IssuesResponse;
+import java.awt.Font;
+import java.io.File;
 
 import java.io.FileNotFoundException;
 import java.util.HashMap;
@@ -15,6 +17,7 @@ import net.sf.jasperreports.engine.JasperCompileManager;
 import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 
 @Service
@@ -26,11 +29,19 @@ public class IssuesReportGenerator {
 
     private JasperPrint getReport(IssuesResponse response) throws FileNotFoundException, JRException {
         List<Issue> issuesList = response.getIssues();
-        Map<String, Object> params = new HashMap<String, Object>();
+        Map<String, Object> params = new HashMap<>();
         params.put("fiscalizadoresData", new JRBeanCollectionDataSource(issuesList));
-        JasperPrint report = JasperFillManager.fillReport(JasperCompileManager.compileReport(
-                ResourceUtils.getFile("classpath:employees.jrxml")
-                        .getAbsolutePath()), params, new JREmptyDataSource());
+
+        // Cargar el archivo .jrxml y compilarlo
+        File reportFile = ResourceUtils.getFile("classpath:employees.jrxml");
+        JasperReport jasperReport = JasperCompileManager.compileReport(reportFile.getAbsolutePath());
+
+        // Registrar la fuente manualmente
+        Font font = new Font("Arial", Font.PLAIN, 12);  // Reemplaza con la fuente que deseas usar
+        params.put("REPORT_FONT", font);
+
+        JasperPrint report = JasperFillManager.fillReport(jasperReport, params, new JREmptyDataSource());
         return report;
     }
+
 }
